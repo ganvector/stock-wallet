ff3bbb17940b52db574a91aa6b125cf60758cf0d
Vitor Nascimento <>
2024-05-03 09:07:10 -0300

    feat: enable server with mongodb local

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-export class Teste {
-  private readonly secret: number = 40;
+import { DefaultCreateCompanyUseCase } from './application/companies/create/DefaultCreateCompanyUseCase';
+import { DefaultGetCompanyByIdUseCase } from './application/companies/retrieve/get/DefaultGetCompanyByIdUseCase';
+import { CompanyController } from './infra/api/company/CompanyController';
+import { Server } from './infra/api/server';
+import { CompanyMongoDBGateway } from './infra/company/persistence/CompanyMongoDBGateway';
+import { MongoDB } from './infra/database/MongoDB';
+import { CONFIG } from './infra/config';
 
-  public check(aTry: number): boolean {
-    return aTry === this.secret;
-  }
+async function bootstrap() {
+  await MongoDB.connect(CONFIG.MONGODB_URI);
+
+  const companyGateway = new CompanyMongoDBGateway();
+  const createCompanyUseCase = new DefaultCreateCompanyUseCase(companyGateway);
+  const getCompanyByIdUseCase = new DefaultGetCompanyByIdUseCase(
+    companyGateway,
+  );
+  const companyController = new CompanyController(
+    createCompanyUseCase,
+    getCompanyByIdUseCase,
+  );
+
+  const server = new Server(companyController);
+
+  server.listen();
 }
 
-export class ClassMate {
-  constructor(
-    private readonly name: string,
-    private readonly age: number,
-  ) {}
-}
+bootstrap();
